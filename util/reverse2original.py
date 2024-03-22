68a8e5cacc3e2a113d905308da971765a4c95701
prayaglehana <prayag.lehana@moonfrog>
2024-03-22 17:17:06 +0530

    reverse

--- util/reverse2original.py
+++ util/reverse2original.py
@@ -170,8 +170,8 @@ def reverse2wholeimage(b_align_crop_tenor_list,swaped_imgs, mats, crop_size, ori
         img = img_mask * target_image + (1-img_mask) * img
         
     final_img = img.astype(np.uint8)
-    # if not no_simswaplogo:
-    #     final_img = logoclass.apply_frames(final_img)
+    if not no_simswaplogo:
+        final_img = logoclass.apply_frames(final_img)
     cv2.imwrite(save_path, final_img)
     retval, buffer = cv2.imencode('.jpg', final_img)
     base64_image = base64.b64encode(buffer).decode('utf-8')
